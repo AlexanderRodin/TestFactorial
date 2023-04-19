434116e056f9d67f2493279a8736f8e5d064f1b4
alexanderrodin <pass.9157@gmail.com>
2023-04-19 20:00:14 +0300

    Sealed class, WithContext and Suspend Coroutines

--- app/src/main/java/com/example/testfactorial/MainActivity.kt
+++ app/src/main/java/com/example/testfactorial/MainActivity.kt
@@ -2,6 +2,9 @@ package com.example.testfactorial
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.lifecycle.ViewModelProvider
 import com.example.testfactorial.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -10,8 +13,41 @@ class MainActivity : AppCompatActivity() {
         ActivityMainBinding.inflate(layoutInflater)
     }
 
+    private val viewModel by lazy {
+        ViewModelProvider(this)[MainViewModel::class.java]
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        observeViewModel()
+        binding.buttonCalculate.setOnClickListener {
+            viewModel.calculate(binding.editTextNumber.text.toString())
+        }
+    }
+
+    private fun observeViewModel() {
+
+        viewModel.state.observe(this) {
+            binding.progressBarLoading.visibility = View.GONE
+            binding.buttonCalculate.isEnabled = true
+
+            when (it) {
+                is Error -> {
+                    Toast.makeText(
+                        this,
+                        "You did not entered value",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+                is Progress -> {
+                    binding.progressBarLoading.visibility = View.VISIBLE
+                    binding.buttonCalculate.isEnabled = false
+                }
+                is Factorial -> {
+                    binding.textViewFactorial.text = it.value
+                }
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/example/testfactorial/MainViewModel.kt
@@ -0,0 +1,57 @@
+package com.example.testfactorial
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.math.BigInteger
+import kotlin.concurrent.thread
+import kotlin.coroutines.suspendCoroutine
+
+class MainViewModel : ViewModel() {
+
+    private val _state = MutableLiveData<State>()
+    val state: LiveData<State>
+        get() = _state
+
+
+    fun calculate(value: String?) {
+        _state.value = Progress
+        if (value.isNullOrBlank()) {
+            _state.value = Error
+            return
+        }
+        viewModelScope.launch {
+            val number = value.toLong()
+            val result = factorial(number)
+            _state.value = Factorial(result)
+        }
+    }
+
+// The first example of implementation through a function with a callback
+
+//    private suspend fun factorial(number: Long): String {
+//        return suspendCoroutine {
+//            thread {
+//                var result = BigInteger.ONE
+//                for (i in 1..number) {
+//                    result *= result.multiply(BigInteger.valueOf(i))
+//                }
+//                it.resumeWith(Result.success(result.toString()))
+//            }
+//        }
+//    }
+
+    private suspend fun factorial(number: Long): String {
+        return withContext(Dispatchers.Default) {
+            var result = BigInteger.ONE
+            for (i in 1..number) {
+                result *= result.multiply(BigInteger.valueOf(i))
+            }
+            result.toString()
+        }
+    }
+}
